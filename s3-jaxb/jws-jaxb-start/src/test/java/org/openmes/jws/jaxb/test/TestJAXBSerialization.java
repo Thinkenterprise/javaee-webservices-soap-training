a4aee992fc1ccb03ea126a97b1a8980083cb73d5
Thinkenterprise <michael.schaefer@msg-systems.com>
2019-12-03 21:46:02 +0100

    JAXB Review

--- s3-jaxb/jws-jaxb-start/src/test/java/org/openmes/jws/jaxb/test/TestJAXBSerialization.java
+++ s3-jaxb/jws-jaxb-start/src/test/java/org/openmes/jws/jaxb/test/TestJAXBSerialization.java
@@ -1,30 +1,21 @@
 package org.openmes.jws.jaxb.test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openmes.jws.jaxb.model.ATMOrder;
-import org.openmes.jws.test.TestModelHelper;
 
 public class TestJAXBSerialization {
-
-	private static JAXBContext context;
+	
+	
+private static JAXBContext context;
 	
 	@BeforeClass
 	public static void iniitializeTest() {
 		
 		try {
-			context = JAXBContext.newInstance(new Class[] {org.openmes.jws.jaxb.model.ATMOrder.class});
+			context = JAXBContext.newInstance(new Class[] {null});
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
@@ -33,36 +24,15 @@ public class TestJAXBSerialization {
 	
 	@Test
 	public void testMarshalling() {
-		Marshaller marshaller;
-		OutputStream outputStream;
-		try {
-			outputStream = new FileOutputStream(new File("xmlStream.xml"));
-			marshaller = context.createMarshaller();
-			marshaller.marshal(TestModelHelper.createRequestObject(), outputStream);
-						
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println();
+		
 	}
 	
 	
 	@Test
 	public void testUnmarshalling() {
-		InputStream inputStream;
-		Unmarshaller unmarshaller;
 		
-		try {
-			inputStream = new FileInputStream(new File("xmlStream.xml"));
-			unmarshaller = context.createUnmarshaller();
-			ATMOrder oder = (ATMOrder)unmarshaller.unmarshal(inputStream);
-					
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 		
 	}
 	
-	
+
 }
